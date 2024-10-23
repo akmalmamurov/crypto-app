ee3cea4f1c597c32247f735ea25b9ced1cd9731e
akoobeeEy <119799055+akoobeeEy@users.noreply.github.com>
2024-10-23 19:08:22 +0500

    suspense addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
+
 import Layout from "@/layout/Layout";
-import Home from "@/pages/Home/Home";
-import Draw from "@/pages/draw/Draw";
-import Friends from "@/pages/friends/Friends";
-import Exchange from "@/pages/exchange/Exchange";
-import Settings from "@/pages/settings/Settings";
-import Mission from "@/pages/mission/Mission";
+const Home = lazy(() => import("@/pages/Home/Home"));
+const Draw = lazy(() => import("@/pages/draw/Draw"));
+const Friends = lazy(() => import("@/pages/friends/Friends"));
+const Exchange = lazy(() => import("@/pages/exchange/Exchange"));
+const Settings = lazy(() => import("@/pages/settings/Settings"));
+const Mission = lazy(() => import("@/pages/mission/Mission"));
 
 function App() {
   return (
-    <Routes>
-
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="draw" element={<Draw />} />
-        <Route path="friends" element={<Friends />} />
-        <Route path="mission" element={<Mission />} />
-        <Route path="exchange" element={<Exchange />} />
-        <Route path="settings" element={<Settings />} />
-      </Route>
-      {/* <Route path="/" element={<Navigate to="/main" replace />} /> */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Suspense fallback={""}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="draw" element={<Draw />} />
+          <Route path="friends" element={<Friends />} />
+          <Route path="mission" element={<Mission />} />
+          <Route path="exchange" element={<Exchange />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        {/* <Route path="/" element={<Navigate to="/main" replace />} /> */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Suspense>
   );
 }
 
